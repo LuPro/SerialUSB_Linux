fcb45134305ba2942628e7b985d2701485ea8674
Luis <luis.immanuel@gmx.at>
2017-10-04 09:27:33 +0200

    removed old commented out code

--- main.cpp
+++ main.cpp
@@ -10,19 +10,6 @@ int main()
 
     Serial serial(portName);
 
-    /*int fd = open (portName, O_RDWR | O_NOCTTY | O_SYNC);
-    if (fd < 0)
-    {
-        cout << "Error opening " << portName << " error code: " << strerror(errno) << endl;
-        return -1;
-    }
-    cout << "Successfully openend " << portName << endl;
-
-    serial.setInterfaceAttribs(fd, B115200, 0);     // set speed to 115,200 bps, 8n1 (no parity)
-    serial.setBlocking(fd, 0);                      // set no blocking
-
-    serial.startRead();*/
-
     while (1) {
         if (serial.bytesAvailable() != 0) {
             cout << to_string(serial.readQueue()) << endl;
